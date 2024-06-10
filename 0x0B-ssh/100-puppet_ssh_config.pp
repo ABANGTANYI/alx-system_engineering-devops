626cdbe12931290472156d86b637cf982918a70c
ABANGTANYI <abangtanyia@gmai.com>
2024-06-10 12:39:30 -0700

    Tasks 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,20 @@
+#!/usr/bin/puppet
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/sshd_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
+
+service { 'ssh':
+  ensure    => 'running',
+  enable    => true,
+  require   => File_line['Turn off passwd auth'],
+  subscribe => File_line['Declare identity file'],
+}
