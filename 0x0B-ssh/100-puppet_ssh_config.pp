6962ba159a3114c0a5a1ed3ee0223044121f51a5
ABANGTANYI <abangtanyia@gmai.com>
2024-06-10 12:51:14 -0700

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,12 @@
-#!/usr/bin/puppet
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
 
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
-}
+file { 'ect/ssh/ssh_config':
+	ensure => present,
 
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile',
-}
-
-service { 'ssh':
-  ensure    => 'running',
-  enable    => true,
-  require   => File_line['Turn off passwd auth'],
-  subscribe => File_line['Declare identity file'],
+content =>"
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
 }
